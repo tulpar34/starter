e66aedf7191864fb0ffd0c2a1edcb0299e6a5fd0
Mustafa Kabalak <85291323+tulpar34@users.noreply.github.com>
2022-11-17 22:18:20 +0300

    ternary conditional örneği olusturuldu

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// bir component olusturalim
+
+function Bilesen({KutuNo}) {
+
+    let sayi = KutuNo
+
+    return (
+        <>
+        <p>Hangi Kutu?</p>
+        {sayi === 2 ? <Kutu2 /> : <Kutu1/>}
+        </>
+        
+        )} /* Ternary Conditioanal */
+
+
+function Kutu1() {
+    return "Ben Kutu1 im"
+}
+function Kutu2() {
+    return "Ben Kutu2 yim"
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <> 
-    <h1>Hello World</h1>
-    
-    
-    </>
+
+    <Bilesen KutuNo = {2}/>
   
 );
 
