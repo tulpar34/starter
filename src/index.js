e7f4dbc976a8f0c021529d0e174ff6c1ea41a3a7
Mustafa Kabalak <85291323+tulpar34@users.noreply.github.com>
2022-11-17 21:48:19 +0300

    react alt eleman(children) ornegi olusturuldu

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
+// bir component oluşturalim
+function Kart(props) {
+    return <p>Hello World {props.children} {props.color}</p>
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <> 
-    <h1>Hello World</h1>
+    <Kart color='red'>
+        { <span>!Mustafa</span> /* bunlar children(alt elemanlar) oluyor Kart'ın içerine yazdığımız her element bunlar componente prop olarak otomatik olarak gönderiliyor 
+        spana erişmek için Kart'a propları parametre olarak aktarmamız gerek
+        */}
+    </Kart>
     
-    
-    </>
-  
 );
 
